7fafa7e97dc142614d95d161dfa4c3ed184b42d3
Allen Ng <ang@appnexus.com>
2017-09-06 21:45:34 -0600

    Added test for StringScanner

--- test/spec/test.js
+++ test/spec/test.js
@@ -23,4 +23,48 @@
             });
         });
     });
+    describe('StringScanner', function(){
+        var testString = 'abc';
+        var scanner;
+        it('should only accept strings', function(){
+            expect(function(){new StringScanner()}).to.throw('Illegal argument');
+            expect(function(){new StringScanner(1)}).to.throw('Illegal argument');
+            expect(function(){new StringScanner({})}).to.throw('Illegal argument');
+            expect(function(){scanner = new StringScanner(testString)}).to.not.throw('Illegal argument');
+        });
+        describe('addToken', function(){
+            it('should be allowed to be added only once', function(){
+                var test = function(){
+                    scanner.addToken('A', 'a');
+                };
+                expect(test).to.not.throw();
+                expect(test).to.throw('Token already added.');
+            });
+        });
+        describe('hasNext', function(){
+            it('should detect if any token matches', function(){
+                assert(scanner.hasNext());
+            });
+            it('should detect if a specific token matches', function(){
+                assert(scanner.hasNextPat('A'));
+            });
+            it('should detect if a given token matches', function(){
+            });
+        });
+        describe('peek', function(){
+            it('should return the next token without advancing', function(){
+                assert(scanner.peek());
+                assert(scanner.peek()===scanner.peek());
+            });
+            it('curToken should be read-only', function(){
+                expect(function(){scanner.curToken='value'}).to.throw('setting a property that has only a getter');
+            });
+        });
+        describe('next', function(){
+            it('should consume and return the next token', function(){
+                var next = scanner.next();
+                assert(next!==undefined);
+            });
+        });
+    });
 })();
